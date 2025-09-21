5232d0106f053eede58f636d07d4679277212dc4
Dhilsen18 <dhilsenamv@gmail.com>
2025-09-21 15:21:55 -0500

    Fix vite.config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss' // Importar el plugin de PostCSS para tailwindcss
-import autoprefixer from 'autoprefixer' // Importar autoprefixer
 
 export default defineConfig({
-  base: '/portfolio-dhilsenmmv/', // Añadir la ruta base para GitHub Pages
   plugins: [
     react(),
   ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(), // Usar el plugin de PostCSS
-        autoprefixer(),
-      ],
-    },
-  },
 })
